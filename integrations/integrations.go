5e46df0eef153a1107e238c7a507340c94f2e895
Yury Gargay <yury.gargay@gmail.com>
2023-10-27 16:26:36 +0200

    Pass JWT Claims Extractor

--- integrations/integrations.go
+++ integrations/integrations.go
@@ -3,8 +3,13 @@ package integrations
 import (
 	"github.com/gorilla/mux"
 	"github.com/netbirdio/netbird/management/server"
+	"github.com/netbirdio/netbird/management/server/jwtclaims"
 )
 
-func RegisterHandlers(router *mux.Router, accountManager server.AccountManager) *mux.Router {
+func RegisterHandlers(
+	router *mux.Router,
+	accountManager server.AccountManager,
+	extractor *jwtclaims.ClaimsExtractor,
+) *mux.Router {
 	return router
 }
