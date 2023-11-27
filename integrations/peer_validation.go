0d0650c8468384764d58a27ed83ac7c72cb29fa6
Pascal Fischer <pascal@netbird.io>
2023-11-27 17:57:33 +0100

    add error to peer update request validation

--- integrations/peer_validation.go
+++ integrations/peer_validation.go
@@ -2,8 +2,8 @@ package integrations
 
 import "github.com/netbirdio/netbird/management/server"
 
-func ValidatePeersUpdateRequest(update *server.Peer, peer *server.Peer, manager server.AccountManager) *server.Peer {
-	return update
+func ValidatePeersUpdateRequest(update *server.Peer, peer *server.Peer, manager server.AccountManager) (*server.Peer, error) {
+	return update, nil
 }
 
 func ValidatePeers(peers []*server.Peer, account *server.Account) []*server.Peer {
