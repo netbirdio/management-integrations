6dc8ff1bf262d550dfb2c646e1557e9dd09e99b1
Pascal Fischer <pascal@netbird.io>
2023-11-28 12:08:44 +0100

    add userId

--- integrations/settings_validation.go
+++ integrations/settings_validation.go
@@ -5,7 +5,7 @@ import (
 	"github.com/netbirdio/netbird/management/server/status"
 )
 
-func ValidateExtraSettings(extraSettings *server.ExtraSettings, account *server.Account, manager server.AccountManager) error {
+func ValidateExtraSettings(extraSettings *server.ExtraSettings, account *server.Account, userID string, manager server.AccountManager) error {
 	if extraSettings != nil {
 		return status.Errorf(status.InvalidArgument, "extra settings are only supported on the cloud version of NetBird")
 	}
