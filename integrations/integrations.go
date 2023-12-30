92debdf072448c8d2d338d6090f43843644ac2af
Maycon Santos <mlsmaycon@gmail.com>
2023-12-30 20:19:00 +0100

    Add InitEventStore function
    extract the method from the management cmd package

--- integrations/integrations.go
+++ integrations/integrations.go
@@ -3,7 +3,10 @@ package integrations
 import (
 	"github.com/gorilla/mux"
 	"github.com/netbirdio/netbird/management/server"
+	"github.com/netbirdio/netbird/management/server/activity"
+	"github.com/netbirdio/netbird/management/server/activity/sqlite"
 	"github.com/netbirdio/netbird/management/server/jwtclaims"
+	log "github.com/sirupsen/logrus"
 )
 
 func RegisterHandlers(
@@ -12,4 +15,17 @@ func RegisterHandlers(
 	extractor *jwtclaims.ClaimsExtractor,
 ) *mux.Router {
 	return router
+}
+
+func InitEventStore(dataDir string, key string) (activity.Store, string, error) {
+	var err error
+	if key == "" {
+		log.Debugf("generate new activity store encryption key")
+		key, err = sqlite.GenerateKey()
+		if err != nil {
+			return nil, "", err
+		}
+	}
+	store, err := sqlite.NewSQLiteStore(dataDir, key)
+	return store, key, err
 }
