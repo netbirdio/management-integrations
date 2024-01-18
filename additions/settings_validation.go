93592c0950a9b697c1977308730dd8c18525e4f4
Maycon Santos <mlsmaycon@gmail.com>
2024-01-18 10:35:57 +0100

    check old extra settings before log

--- additions/settings_validation.go
+++ additions/settings_validation.go
@@ -9,7 +9,9 @@ import (
 
 func ValidateExtraSettings(newExtraSettings *account.ExtraSettings, oldExtraSettings *account.ExtraSettings, peers map[string]*nbpeer.Peer, userID string, accountID string, eventStore activity.Store) error {
 	if newExtraSettings != nil {
-		log.Info("extra settings are only supported on the cloud version of NetBird")
+		if oldExtraSettings != nil {
+			log.Info("extra settings are only supported on the cloud version of NetBird")
+		}
 		if newExtraSettings.PeerApprovalEnabled {
 			log.Info("setting peer approval to false")
 			newExtraSettings.PeerApprovalEnabled = false
