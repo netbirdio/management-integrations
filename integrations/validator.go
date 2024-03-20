258ecd390e1290ef34e564bf2f80a43b751bdf30
Zoltán Papp <zoltan.pmail@gmail.com>
2024-03-20 13:44:57 +0100

    Update IntegratedValidator default implementation

--- integrations/validator.go
+++ integrations/validator.go
@@ -3,6 +3,7 @@ package integrations
 import (
 	"github.com/netbirdio/netbird/management/server/account"
 	"github.com/netbirdio/netbird/management/server/activity"
+	"github.com/netbirdio/netbird/management/server/group"
 	nbpeer "github.com/netbirdio/netbird/management/server/peer"
 )
 
@@ -29,7 +30,7 @@ func (v *IntegratedValidatorImpl) IsNotValidPeer(_ string, _ *nbpeer.Peer, _ []s
 	return false
 }
 
-func (v *IntegratedValidatorImpl) GetValidatedPeers(_ string, _ map[string][]string, peers map[string]*nbpeer.Peer, _ *account.ExtraSettings) (map[string]struct{}, error) {
+func (v *IntegratedValidatorImpl) GetValidatedPeers(_ string, _ map[string]*group.Group, peers map[string]*nbpeer.Peer, _ *account.ExtraSettings) (map[string]struct{}, error) {
 	validatedPeers := make(map[string]struct{})
 	for p := range peers {
 		validatedPeers[p] = struct{}{}
