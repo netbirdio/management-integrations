ceb1013bf86187d38f095b39d640ce929c4aaac0
Pascal Fischer <pascal@netbird.io>
2025-01-31 13:27:30 +0100

    add proxy controller

---
+++ integrations/port_forwarding.go
@@ -0,0 +1,27 @@
+package integrations
+
+import (
+	"context"
+
+	"github.com/netbirdio/netbird/management/server/integrations/port_forwarding"
+	"github.com/netbirdio/netbird/management/server/store"
+	"github.com/netbirdio/netbird/management/server/types"
+)
+
+type controllerImpl struct {
+	store store.Store
+}
+
+func NewController(store store.Store) port_forwarding.Controller {
+	return &controllerImpl{
+		store: store,
+	}
+}
+
+func (c *controllerImpl) SendUpdate(ctx context.Context, accountID string, affectedProxyID string, affectedPeerIDs []string) {
+	// noop
+}
+
+func (c *controllerImpl) GetProxyNetworkMaps(ctx context.Context, accountID string) (map[string]*types.NetworkMap, error) {
+	return make(map[string]*types.NetworkMap), nil
+}
