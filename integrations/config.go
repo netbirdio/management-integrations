21d4cd97a17b1eae6a565ca4cca3c1e6f11a25ce
Pascal Fischer <pascal@netbird.io>
2025-02-26 17:07:42 +0100

    add config

---
+++ integrations/config.go
@@ -0,0 +1,10 @@
+package integrations
+
+import (
+	"github.com/netbirdio/netbird/management/proto"
+	"github.com/netbirdio/netbird/management/server/account"
+)
+
+func ExtendNetBirdConfig(config *proto.NetbirdConfig, extraSettings *account.ExtraSettings) {
+	return
+}
