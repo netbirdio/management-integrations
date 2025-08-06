fda9b40390415c34a7cebf7c3fa37c1d49cd4fc7
bcmmbaga <bethuelmbaga12@gmail.com>
2025-08-06 09:47:27 +0300

    go mod tidy

--- integrations/config/config.go
+++ integrations/config/config.go
@@ -1,8 +1,8 @@
 package config
 
 import (
-	"github.com/netbirdio/netbird/shared/management/proto"
 	"github.com/netbirdio/netbird/management/server/types"
+	"github.com/netbirdio/netbird/shared/management/proto"
 )
 
 func ExtendNetBirdConfig(peerID string, peerGroups []string, config *proto.NetbirdConfig, extraSettings *types.ExtraSettings) *proto.NetbirdConfig {
